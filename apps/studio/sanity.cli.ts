fc93c94459e394d7464c3232617ca247147c31af
Brett Gamble <brettkgamble@gmail.com>
2025-03-27 11:21:28 -0600

    0001 - First commit

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -11,7 +11,7 @@ export default defineCliConfig({
   },
   studioHost:
     host && host !== "main"
-      ? `${host}-turbo-start-sanity`
-      : "turbo-start-sanity",
+      ? `${host}-precious-metals-recovery`
+      : "precious-metals-recovery",
   autoUpdates: false,
 });
